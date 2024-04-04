3070f632de2abed7d124854f815f2d838884c606
Sofiia Prokhorets <153623660+iamscd@users.noreply.github.com>
2024-04-04 20:59:11 +0100

    moved pics

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socket = require("socket.io");
 
 // App setup
-const PORT = 5000;
+const PORT = 3000;
 const app = express();
 const server = app.listen(PORT, function () {
     console.log(`^Listening on port ${PORT}`);
